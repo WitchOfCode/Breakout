79159d9c4ef7102e1dc65af11ac763519d79529e
WitchOfCode <108596415+WitchOfCode@users.noreply.github.com>
2024-05-24 22:17:03 -0400

    delete bricks.py

--- bricks.py
+++ bricks.py
@@ -1,12 +1,1 @@
-import pygame
-from config import *
-import time
 
-
-class Bricks:
-    def __init__(self, x, y, color=RED):
-        self.rect = pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
-        self.color = color
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect)
