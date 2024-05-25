a7d050e8062d558ff78725f46b0c9022b8136b9e
WitchOfCode <108596415+WitchOfCode@users.noreply.github.com>
2024-05-24 22:25:21 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -14,7 +14,7 @@ class Ball(pygame.sprite.Sprite):
  
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         
-        self.velocity = [randint(4,8),randint(-8,8)]
+        self.velocity = [randint(2,4),randint(-4,4)]
         
         self.rect = self.image.get_rect()
         
@@ -24,6 +24,6 @@ class Ball(pygame.sprite.Sprite):
           
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
-        self.velocity[1] = randint(-8,8), self.radius
+        self.velocity[1] = randint(-8,8)
 
 
