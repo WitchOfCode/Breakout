a968efe958d5bf6ddd3f3e9e171531628e46442a
WitchOfCode <108596415+WitchOfCode@users.noreply.github.com>
2024-05-24 22:54:14 -0400

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,5 +1,7 @@
 import pygame
+
 BLACK = (0,0,0)
+
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
@@ -8,11 +10,24 @@ class Paddle(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
+
     def moveLeft(self, pixels):
         self.rect.x -= pixels
         if self.rect.x < 0:
             self.rect.x = 0
+
     def moveRight(self, pixels):
         self.rect.x += pixels
         if self.rect.x > 700:
             self.rect.x = 700
+
+    def collide_with_ball(self, ball):
+        if ball.rect.colliderect(self.rect):
+            if ball.rect.right >= self.rect.left and ball.rect.left < self.rect.left:
+                ball.velocity[1] = -ball.velocity[1]
+                ball.rect.right = self.rect.left - 1
+            elif ball.rect.left <= self.rect.right and ball.rect.right > self.rect.right:
+                ball.velocity[1] = -ball.velocity[1]
+                ball.rect.left = self.rect.right + 1
+            else:
+                ball.bounce()
